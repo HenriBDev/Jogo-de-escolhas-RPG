2fd8f5a126ca15102534c45352f33c7ae9bd5d34
DmN9x <marcusgosr@gmail.com>
2021-09-08 18:37:30 -0300

    :sparkles: feat Implementações Visuais Win/Lose
    Adicionado funções de vitória e derrota para a limpeza do código
    Adicionado efeitos visuais de vitória e derrota (Troféu e Caveira)
    Adicionado a esquematização de vidas

--- game.py
+++ game.py
@@ -4,13 +4,51 @@ import os
 os.system('CLS')
 tentativas = 0
 sala = 1
+vidas = 8
 CAMINHO_VERMELHO = 1
 CAMINHO_PRETO = 2
 
-while (tentativas < 7 and sala != 9):
-    tentativas += 1
-    print("Você está na sala: {}\nEscolha seu caminho".format(sala))
+def narrador(sala):       
+    print("Você tem {} vidas restantes".format(vidas))
+    print("Você está na sala: {}\nEscolha seu caminho".format(sala)) 
     print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
+
+def mensagemDerrota():
+    print("Você gastou todos seus recursos, não há mais saidas, você e seus companheiros morreram")
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def mensagemVitoria():
+    print("Você está na sala: {}\nVocê venceu!!".format(sala))
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+while (tentativas <= 7 and sala != 9):
+    tentativas += 1
+    vidas -= 1
+    narrador(sala)
 
     escolha = int(input())
 
@@ -18,13 +56,13 @@ while (tentativas < 7 and sala != 9):
         sala += escolha
         os.system('CLS')
     elif sala == 8:
-        sala = randrange(1, 5)
+        sala = randrange(1, 6)
         os.system('CLS')
     else:
         print("\nNão existe esse caminho!\n\n")
 
-
-if tentativas >= 7:
-    print("Você gastou todos seus recursos, não há mais saidas, você e seus companheiros morreram")
+if tentativas > 7:
+    mensagemDerrota()
 else:
-    print("Você está na sala: {}\nVocê venceu!!".format(sala))
+    mensagemVitoria()
+
