ac1c4c2c035dd2429af478ff2a8b6e832caeabaf
HenriBDev <henribdev@gmail.com>
2021-09-08 18:29:43 -0300

    :bug: FIX Correção do Código Principal
    - Criação das constantes que indicam o valor de ambos os caminhos.
    - Alteração na condição do laço responsável pela escolha dos caminhos.
    - Alteração nos desvios condicionais para os casos das salas 6 e 8.
    - Alteração nos desvios condicionais ao fim do laço.

--- game.py
+++ game.py
@@ -1,42 +1,30 @@
 from random import randrange
-
 import os
 
-
-def dungeon(tentativas, sala):
-
-    while tentativas <= 7 and sala <= 7:
-        print("Você está na sala: {}\nEscolha seu caminho".format(sala))
-
-        print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
-
-        escolha = int(input())
-
-        if escolha == 1:
-            sala += 1
-        elif escolha == 2:
-            sala += 2
-        else:
-            ("Não existe esse caminho!")
-        os.system('CLS')
-        tentativas += 1
-
-    return tentativas, sala
-
-
 os.system('CLS')
-
-tentativas = 1
+tentativas = 0
 sala = 1
+CAMINHO_VERMELHO = 1
+CAMINHO_PRETO = 2
 
-tentativas, sala = dungeon(tentativas, sala)
+while (tentativas < 7 and sala != 9):
+    tentativas += 1
+    print("Você está na sala: {}\nEscolha seu caminho".format(sala))
+    print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
+
+    escolha = int(input())
+
+    if ((escolha == CAMINHO_VERMELHO and sala != 6) or (escolha == CAMINHO_PRETO and sala != 8)):
+        sala += escolha
+        os.system('CLS')
+    elif sala == 8:
+        sala = randrange(1, 5)
+        os.system('CLS')
+    else:
+        print("\nNão existe esse caminho!\n\n")
+
 
 if tentativas >= 7:
     print("Você gastou todos seus recursos, não há mais saidas, você e seus companheiros morreram")
-elif sala == 9:
+else:
     print("Você está na sala: {}\nVocê venceu!!".format(sala))
-elif sala == 8:
-    sala = randrange(1, 6)
-
-    tentaticas = 1
-    tentativas, sala = dungeon(tentativas, sala)
