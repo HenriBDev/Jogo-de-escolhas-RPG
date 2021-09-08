f0b79791a9e618c810cc61e739f05d756f7a4c2b
1dreambr <lelemaynard50@gmail.com>
2021-09-08 18:55:43 -0300

    :sparkles: feat Adição de Recursos Gráficos
    função adicionada: vida (<3)
    remodelação de variáveis
    conserto da condição de vitória/derrota

--- game.py
+++ game.py
@@ -2,35 +2,42 @@ from random import randrange
 import os
 
 os.system('CLS')
-tentativas = 0
+tentativas = 7
 sala = 1
-vidas = 8
 CAMINHO_VERMELHO = 1
 CAMINHO_PRETO = 2
 
-def narrador(sala):       
-    print("Você tem {} vidas restantes".format(vidas))
-    print("Você está na sala: {}\nEscolha seu caminho".format(sala)) 
+
+def mensagemNarrador(sala):
+    print("\nVocê está na sala: {}\nEscolha seu caminho".format(sala))
     print("[1] - Caminho Vermelho\n[2] - Caminho Preto")
+
+
+def mostraVida(vidas):
+    cont = vidas
+    while(cont > 0):
+        print("<3 ", end="")
+        cont -= 1
+
 
 def mensagemDerrota():
     print("Você gastou todos seus recursos, não há mais saidas, você e seus companheiros morreram")
-    print("    _______________         ")
-    print("   /               \       ")
-    print("  /                 \      ")
-    print("//                   \/\  ")
-    print("\|   XXXX     XXXX   | /   ")
-    print(" |   XXXX     XXXX   |/     ")
-    print(" |   XXX       XXX   |      ")
-    print(" |                   |      ")
-    print(" \__      XXX      __/     ")
-    print("   |\     XXX     /|       ")
-    print("   | |           | |        ")
-    print("   | I I I I I I I |        ")
-    print("   |  I I I I I I  |        ")
-    print("   \_             _/       ")
-    print("     \_         _/         ")
-    print("       \_______/           ")
+    print("     _______________         ")
+    print("    /               \       ")
+    print("   /                 \      ")
+    print("/\/                   \/\  ")
+    print("\ |   XXXX     XXXX   | /   ")
+    print(" \|   XXXX     XXXX   |/     ")
+    print("  |   XXX       XXX   |      ")
+    print("  |                   |      ")
+    print("  \__      XXX      __/     ")
+    print("    |\     XXX     /|       ")
+    print("    | |           | |        ")
+    print("    | I I I I I I I |        ")
+    print("    |  I I I I I I  |        ")
+    print("    \_             _/       ")
+    print("      \_         _/         ")
+    print("        \_______/           ")
 
 def mensagemVitoria():
     print("Você está na sala: {}\nVocê venceu!!".format(sala))
@@ -45,10 +52,10 @@ def mensagemVitoria():
     print("         _.' '._        ")
     print("        '-------'       ")
 
-while (tentativas <= 7 and sala != 9):
-    tentativas += 1
-    vidas -= 1
-    narrador(sala)
+while (tentativas > 0 and sala != 9):
+
+    mostraVida(tentativas)
+    mensagemNarrador(sala)
 
     escolha = int(input())
 
@@ -61,8 +68,9 @@ while (tentativas <= 7 and sala != 9):
     else:
         print("\nNão existe esse caminho!\n\n")
 
-if tentativas > 7:
+    tentativas -= 1
+
+if(tentativas == 0 and sala != 9):
     mensagemDerrota()
 else:
     mensagemVitoria()
-
